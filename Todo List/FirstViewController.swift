2febb5f53894a8f63b52a9509b16987db1348068
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-07-17 11:46:47 +0700

    Another commit and an initial push

--- Todo List/FirstViewController.swift
+++ Todo List/FirstViewController.swift
@@ -8,18 +8,82 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var arrayOfTodos = [String]()
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var todoListTableView: UITableView!
+    
+//    func editButtonPressed() {
+//        todoListTableView.setEditing(!todoListTableView.editing, animated: true)
+//        if todoListTableView.editing {
+//            self.navigationItem.rightBarButtonItem?.title = "Done"
+//        }
+//        else {
+//            self.navigationItem.rightBarButtonItem?.title = "Edit"
+//        }
+//    }
+    
+    // MARK: - UIViewController methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: UIBarButtonItemStyle.Plain, target: self, action: "editButtonPressed")
+        navigationItem.leftBarButtonItem = editButtonItem()
+        if let todoListSavedData = NSUserDefaults.standardUserDefaults().objectForKey("todoListSavedData") {
+            arrayOfTodos = todoListSavedData as! [String]
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        todoListTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func setEditing(editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        todoListTableView.setEditing(editing, animated: animated)
+    }
+    
+    // MARK: - UITableViewDataSource Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfTodos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let eachCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "prototypeCell")
+        eachCell.textLabel!.text = arrayOfTodos[indexPath.row]
+        return eachCell
+    }
+    
+    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            arrayOfTodos.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(arrayOfTodos, forKey: "todoListSavedData")
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+    
+    // Override to support rearranging the table view.
+    func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        
+    }
+    
+    // Override to support conditional rearranging of the table view.
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
 }
 
--- Todo List/SecondViewController.swift
+++ Todo List/SecondViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - Properties
 
+    @IBOutlet weak var addTodoButton: UIButton!
+    @IBOutlet weak var todoItemTextField: UITextField!
+    @IBAction func addTodoButton(sender: UIButton) {
+        isTodoEntryValid()
+        arrayOfTodos.append(todoItemTextField.text!)
+        NSUserDefaults.standardUserDefaults().setObject(arrayOfTodos, forKey: "todoListSavedData")
+        todoItemTextField.text = ""
+        addTodoButton.enabled = false
+    }
+    
+    // MARK: - Custom methods
+    
+    func isTodoEntryValid() {
+        if todoItemTextField.text!.isEmpty {
+            addTodoButton.enabled = false
+        }
+        else {
+            addTodoButton.enabled = true
+        }
+    }
+    
+    // MARK: - UIViewController methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        todoItemTextField.delegate = self
+        addTodoButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: - UITextField Delegate methods
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        addTodoButton.enabled = true
+        todoItemTextField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        addTodoButton.enabled = false
+    }
+    
+    // MARK: - UIResponder methods
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+        isTodoEntryValid()
+    }
 }
 
